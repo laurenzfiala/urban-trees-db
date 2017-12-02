3ae29c8afdc86d2c19e681f91d83e95d21866ca3
laurenzfiala <laurenz.fiala@outlook.at>
2017-12-02 13:14:36 +0100

    fixed wrong fk in phen_obs_obj, added some sample data insetion scripts

---
+++ source/schemas/tree_data/_sampledata/phenology_observation_type.sql
@@ -0,0 +1,5 @@
+insert into tree_data.phenology_observation_type
+(ID, "type", cre_dat, mod_dat, cre_usr, mod_usr)
+values 
+	(9990, 'Bark (Sample)', now(), now(), user, user),
+	(9991, 'Moss (Sample)', now(), now(), user, user);
